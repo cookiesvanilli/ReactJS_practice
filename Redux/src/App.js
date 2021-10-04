7b57c91f969059e51d4386ef11959c7e9df30047
cookies <in.d2010@yandex.ru>
2021-10-04 23:01:07 +0300

    redux

---
+++ Redux/src/App.js
@@ -0,0 +1,14 @@
+import In from './In';
+import Out from './Out';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<In/>
+<Out/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Redux/src/app/taskReducerSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const taskReducerSlice = createSlice({
+  name: 'store',
+  initialState: {
+    value: 0,
+    text : 'privet'
+  },
+  reducers: {
+    add: (state, data) => {
+      state.value += +data.payload;
+    },
+    minus: (state, data) => {
+      state.value -= +data.payload;
+    },
+    multi: (state, data) => {
+        state.value *= +data.payload;
+    },
+    division: (state, data) => {
+        state.value /= +data.payload;
+    },
+    textInput: (state, data) => {
+        state.text = data.payload;
+    }
+  },
+});
+
+export const { add, minus, multi, division, textInput, showConsole } = taskReducerSlice.actions;
+
+export const selectValue = state => state.taskReducer.value;
+export const selectTest = state => state.taskReducer.text;
+
+export default taskReducerSlice.reducer;
